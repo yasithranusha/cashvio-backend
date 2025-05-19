eca175b647608134033e2e2a71e4c5e3d477611e
Yasith Ranusha Silva <yasithranusha24@gmail.com>
2025-05-20 00:01:05 +0530

    fix: order service rabbitmq

--- apps/order/src/constants/services.ts
+++ apps/order/src/constants/services.ts
@@ -1,1 +1,1 @@
-export const MAILER_SERVICE = 'MAILER_SERVICE';
+export const MAILER_SERVICE = 'MAILER';
--- apps/order/src/order.module.ts
+++ apps/order/src/order.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import * as Joi from 'joi';
-import { DatabaseModule } from '@app/common';
+import { DatabaseModule, RmqModule } from '@app/common';
 import { AuthModule } from '@app/common';
 import { OrderController } from './order.controller';
 import { OrderService } from './order.service';
 import { validate } from './env.validation';
 import { CustomerOrderModule } from './customer-order/customer-order.module';
 import { ShopBalanceModule } from './shop-balance/shop-balance.module';
+import { MAILER_SERVICE } from './constants/services';
+import { ReceiptPdfService } from './pdf/receipt-pdf.service';
 
 @Module({
   imports: [
@@ -20,17 +22,22 @@ import { ShopBalanceModule } from './shop-balance/shop-balance.module';
         AWS_SECRET_ACCESS_KEY: Joi.string().required(),
         AWS_KMS_KEY_ID: Joi.string().optional(),
         AWS_KMS_KEY_ALIAS: Joi.string().optional(),
+        RABBIT_MQ_URI: Joi.string().required(),
+        RABBIT_MQ_ORDER_QUEUE: Joi.string().required(),
+        RABBIT_MQ_MAILER_QUEUE: Joi.string().required(),
       }),
       envFilePath: ['./apps/order/.env'],
       validate,
     }),
     AuthModule,
     DatabaseModule,
+    RmqModule,
+    RmqModule.register({ name: MAILER_SERVICE }),
     CustomerOrderModule,
     ShopBalanceModule,
   ],
   controllers: [OrderController],
-  providers: [OrderService],
+  providers: [OrderService, ReceiptPdfService],
   exports: [OrderService],
 })
 export class OrderModule {}
